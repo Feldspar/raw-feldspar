bc086ac826878d451f790153c2f5fbeb3b2ada57
Markus <mararon@chalmers.se>
2016-01-28 17:44:01 +0100

    short-hand for matrices over feldspar expressions

--- examples/LDPC.hs
+++ examples/LDPC.hs
@@ -3,12 +3,16 @@ module LDPC where
 import Feldspar
 
 --------------------------------------------------------------------------------
--- * ...
+-- * Matrices.
 --------------------------------------------------------------------------------
 
 -- | Representation of matrices as a row & col count and indexing function.
 data Matrix a = Matrix (Data Index) (Data Index) (Data Index -> Data Index -> a)
 
+-- | Short-hand for matrices over Feldspar expressions.
+type Mat a = Matrix (Data a)
+
+-- | Storable instance to declare memory representation of matrices.
 instance Type a => Storable (Matrix (Data a))
   where
     type StoreRep (Matrix (Data a)) = (Ref Length, Ref Length, Arr a)
@@ -39,12 +43,28 @@ instance Type a => Storable (Matrix (Data a))
          setRef cd cols
          copyArr dst src (rows * cols)
 
+-- | Create a matrix of some row & col count 
 freezeMatrix :: Type a => Data Length -> Data Length -> Arr a -> Matrix (Data a)
 freezeMatrix rows cols array = Matrix rows cols $ \r c -> unsafeArrIx array (r * cols + c)
 
 --------------------------------------------------------------------------------
--- **
+-- ** Find an LU decomposition of a sparse matrix.
 
--- ..
+decompose
+  :: Mat a   -- ^ Input matrix, M by N.
+  -> Int     -- ^ K
+  -> Int     -- ^ Size of sub-matrix to find LU decomposition of
+  -> Int     -- ^ Number of columns to abandom.
+  -> Int     -- ^ Whent to abandom said columns.
+  -> ( Mat a -- ^ L matrix, M by K.
+     , Mat a -- ^ U matrix, K by N.
+     )
+decompose = undefined
+
+--------------------------------------------------------------------------------
+-- * Encoding.
+--------------------------------------------------------------------------------
+
+
 
 --------------------------------------------------------------------------------
