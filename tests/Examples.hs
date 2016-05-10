c43b8bf55fbfda3648a0948337b7099e94765fa7
Emil Axelsson <emax@chalmers.se>
2016-05-10 18:50:14 +0200

    Fix FFT tests now that floating point values are printed/scanned correctly

--- tests/Examples.hs
+++ tests/Examples.hs
@@ -13,23 +13,29 @@ import qualified Concurrent
 import DFT
 import FFT
 
+
+
+almostEq a b
+    =          Complex.magnitude d Prelude.< 1e-7
+    Prelude.&& Complex.phase d     Prelude.< 1e-7
+  where
+    d = abs (a-b)
+
+a ~= b = Prelude.and $ Prelude.zipWith almostEq a b
+
 prop_fft_dft dft' fft' = QC.monadicIO $ do
     n   :: Int              <- QC.pick $ QC.choose (2,5)
     inp :: [Complex Double] <- QC.pick $ QC.vector (2 Prelude.^ n)
     outd <- QC.run $ dft' inp
     outf <- QC.run $ fft' inp
-    QC.assert (outd Prelude.== outf)
-
-a ~= b = Complex.magnitude d Prelude.< 1e-3 Prelude.&& Complex.phase d Prelude.< 1e-3
-  where
-    d = abs (a-b)
+    QC.assert (outd ~= outf)
 
 prop_inverse f fi = QC.monadicIO $ do
     n   :: Int              <- QC.pick $ QC.choose (2,5)
     inp :: [Complex Double] <- QC.pick $ QC.vector (2 Prelude.^ n)
     out1 <- QC.run $ f inp
     out2 <- QC.run $ fi out1
-    QC.assert (Prelude.and $ Prelude.zipWith (~=) inp out2)
+    QC.assert (inp ~= out2)
 
 testFFT =
     marshalledM (return . dft) $ \dft' ->
