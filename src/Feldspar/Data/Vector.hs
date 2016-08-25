d8f6338a0fc5faaf626be7c5893b93f47ae6478c
Emil Axelsson <emax@chalmers.se>
2016-08-25 06:25:36 +0200

    Creds and references for vector library

--- src/Feldspar/Data/Vector.hs
+++ src/Feldspar/Data/Vector.hs
@@ -163,6 +163,19 @@ import qualified Language.Embedded.Concurrent as Imp
 
 
 
+-- This library has been inspired by the vector library in feldspar-language:
+-- <https://github.com/Feldspar/feldspar-language/blob/master/src/Feldspar/Vector.hs>
+--
+-- The general idea of pull and push vectors is described in
+-- "Combining deep and shallow embedding of domain-specific languages"
+-- <http://dx.doi.org/10.1016/j.cl.2015.07.003>.
+--
+-- Push arrays were originally introduced in
+-- "Expressive array constructs in an embedded GPU kernel programming language"
+-- <http://dx.doi.org/10.1145/2103736.2103740>.
+
+
+
 --------------------------------------------------------------------------------
 -- * Generic operations
 --------------------------------------------------------------------------------
